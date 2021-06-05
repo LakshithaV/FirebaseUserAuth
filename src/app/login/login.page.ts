d98a837a1033a985bd13d1f2ecb5d454bfdbec55
LakshithaV <lakshithaviraj@hotmail.com>
2021-06-06 00:05:56 +0530

    Register and Login Page Created

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(
+    private auth: AuthService,
+    private toaster: ToastController
+  ) { }
 
   ngOnInit() {
   }
+
+  login(){
+    if(this.email && this.password){
+      this.auth.signIn(this.email, this.password);
+    } else {
+      this.toast('Please enter your email & password!', 'warning');
+    }
+  }
+
+  async toast(message, status){
+    const toast = await this.toaster.create({
+      message: message,
+      color: status,
+      position: 'top',
+      duration: 2000
+    });
+    toast.present();
+  }
+  
 
 }
